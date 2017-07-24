c348c12030d2ad18475cf12000a562ef5176e844
arthur <arturk.evgenievich@gmail.com>
2017-07-24 17:13:04 +0300

    dropdown with races

--- src/main/java/com/ak/controller/Controller.java
+++ src/main/java/com/ak/controller/Controller.java
@@ -1,6 +1,16 @@
 package com.ak.controller;
 
+import com.ak.entity.Race;
+import com.ak.service.RaceService;
+import com.ak.toolkits.CommonToolkit;
+import com.ak.toolkits.CustomUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by arthur on 7/20/17.
@@ -8,8 +18,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    @RequestMapping("/")
-    public String home(){
+    @Autowired
+    RaceService raceService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+        List<Race> races = new ArrayList<>();
+        CustomUser customUser = CommonToolkit.getCustomUser();
+        if (customUser != null)
+            races.addAll(raceService.getRacesById(customUser.getId()));
+
+        model.addAttribute("races", races);
         return "/home";
     }
 }
