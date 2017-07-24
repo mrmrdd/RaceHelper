6f27db936f05e4727c9825b7667b62f3cb91bde7
arthur <arturk.evgenievich@gmail.com>
2017-07-24 17:10:39 +0300

    get race by user id

--- src/main/java/com/ak/dao/RaceDao.java
+++ src/main/java/com/ak/dao/RaceDao.java
@@ -3,6 +3,10 @@ package com.ak.dao;
 
 import com.ak.entity.Race;
 
+import java.util.List;
+
 public interface RaceDao {
     void addRace(Race race);
+
+    List getRacesById(int id);
 }
--- src/main/java/com/ak/dao/RaceDaoImpl.java
+++ src/main/java/com/ak/dao/RaceDaoImpl.java
@@ -1,10 +1,13 @@
 package com.ak.dao;
 
 import com.ak.entity.Race;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class RaceDaoImpl implements RaceDao {
@@ -15,5 +18,12 @@ public class RaceDaoImpl implements RaceDao {
     @Override
     public void addRace(Race race) {
        em.persist(race);
+    }
+
+    @Override
+    public List<Race> getRacesById(int id) {
+        return em.createQuery("from Race where uzer_id=:id", Race.class)
+                .setParameter("id", id)
+                .getResultList();
     }
 }
