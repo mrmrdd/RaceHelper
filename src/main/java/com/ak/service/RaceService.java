ae42f4e46751eab8813c2e0a6002204abd108e5d
arthur <arturk.evgenievich@gmail.com>
2017-07-24 17:16:58 +0300

    get races by id

--- src/main/java/com/ak/service/RaceService.java
+++ src/main/java/com/ak/service/RaceService.java
@@ -2,9 +2,12 @@ package com.ak.service;
 
 import com.ak.entity.Race;
 
+import java.util.List;
+
 /**
  * Created by arthur on 7/21/17.
  */
 public interface RaceService {
     void addRace(Race race);
+    List getRacesById(int id);
 }
--- src/main/java/com/ak/service/RaceServiceImpl.java
+++ src/main/java/com/ak/service/RaceServiceImpl.java
@@ -7,16 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class RaceServiceImpl implements RaceService{
 
     @Autowired
-    RaceDao raceDao;
+    private RaceDao raceDao;
 
     @Override
     @Transactional
     public void addRace(Race race) {
         raceDao.addRace(race);
+    }
+
+    @Override
+    public List<Race> getRacesById(int id) {
+        return raceDao.getRacesById(id);
     }
 }
