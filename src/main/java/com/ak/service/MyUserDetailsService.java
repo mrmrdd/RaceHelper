61d298ff4f8a5a0592693f1a78ed640e6fbbe216
arthur <arturk.evgenievich@gmail.com>
2017-07-24 17:17:08 +0300

    refactor

--- src/main/java/com/ak/service/MyUserDetailsService.java
+++ src/main/java/com/ak/service/MyUserDetailsService.java
@@ -1,15 +1,18 @@
 package com.ak.service;
 
 import com.ak.entity.Uzer;
+import com.ak.toolkits.CustomUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -26,6 +29,6 @@ public class MyUserDetailsService implements UserDetailsService {
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
         grantedAuthorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
-        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), grantedAuthorities);
+        return new CustomUser(user.getName(), user.getPassword(), grantedAuthorities, user.getId());
     }
 }
